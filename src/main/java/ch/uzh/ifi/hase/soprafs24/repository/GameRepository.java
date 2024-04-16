626cc776afd9e6847dba36ef1158965d0b140ce7
Skyl3ss <andri-spescha@swissonline.ch>
2024-04-17 01:18:27 +0200

    feat: added ability to subscribe to changes and listen to websockets

--- src/main/java/ch/uzh/ifi/hase/soprafs24/repository/GameRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/repository/GameRepository.java
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.hase.soprafs24.repository;
 
 import ch.uzh.ifi.hase.soprafs24.entity.Game;
+import ch.uzh.ifi.hase.soprafs24.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,6 @@ import java.util.UUID;
 
 @Repository
 public interface GameRepository extends JpaRepository<Game, UUID> {
+    Game getGameById(UUID gameId);
+
 }
