37178debad9e74565f543429c4901b7c6b5a573a
Skyl3ss <andri-spescha@swissonline.ch>
2024-04-23 23:34:11 +0200

    feat: added distribution of picture id to clients

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/RoundStompController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/RoundStompController.java
@@ -1,9 +1,5 @@
 package ch.uzh.ifi.hase.soprafs24.controller;
 
-import ch.uzh.ifi.hase.soprafs24.entity.User;
-import ch.uzh.ifi.hase.soprafs24.entity.Game;
-import ch.uzh.ifi.hase.soprafs24.rest.dto.GameGetDTO;
-import ch.uzh.ifi.hase.soprafs24.rest.mapper.DTOMapper;
 import ch.uzh.ifi.hase.soprafs24.service.*;
 import ch.uzh.ifi.hase.soprafs24.service.RoundService;
 import org.springframework.stereotype.Controller;
@@ -11,16 +7,15 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
+import org.json.JSONObject;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.UUID;
 
 
@@ -41,94 +36,45 @@ public class RoundStompController {
         this.messagingTemplate = messagingTemplate;
     }
 
-    @MessageMapping("/games/{gameId}/spiedObject")
-    public void handleSpiedObject(SpiedObjectIn spiedObjectIn, @DestinationVariable("gameId") int gameId) {
-        //extract information from JSON
-        String keyword = spiedObjectIn.getObject();
-        String color = spiedObjectIn.getColor();
-        Location location = spiedObjectIn.getLocation();
-        logger.info("Received spiedObject with keyword {} for game {}", keyword, gameId);
+    int i = 0;
+    @MessageMapping("/games/{gameId}/round")
+    public void getLobbyInformation(@DestinationVariable("gameId") UUID gameId){
+        i++;
+        if(i>=2){
+            try {
+                // Construct URL
+                String apiUrl = "https://api.unsplash.com/photos/random";
+                String query = "Switzerland+landscape+cityscape";
+                String clientId = "Ri4Er_Nr9GAMk_QTpErZvHuVESEVmM7RNKnymwEcNZM"; // Replace with your Unsplash access key
 
-        //save information of spied object
-        RoundService.saveSpiedObjectInfo(gameId, keyword);
+                String urlString = apiUrl + "?query=" + query;
 
-        // Save time as start time of the round
-        Date startTime = RoundService.initializeStartTime(gameId);
+                // Create HttpClient
+                HttpClient client = HttpClient.newHttpClient();
 
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String startTimeString = dateFormat.format(startTime);
+                // Create HttpRequest
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(new URI(urlString))
+                        .header("Authorization", "Client-ID " + clientId)
+                        .build();
 
-        float duration = RoundService.getDuration(gameId); //duration in minutes
+                // Send request and get response
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+// Parse JSON response
+                JSONObject jsonResponse = new JSONObject(response.body());
 
-        //return SpiedObjectOut to subscribers
-        webSocketService.sendMessageToSubscribers("/topic/games/"+gameId+"/spiedObject", new SpiedObjectOut(location, color, startTimeString, duration));
-        RoundService.runTimer(this, gameId);
-    }
+                // Extract ID
+                String id = jsonResponse.getString("id");
 
-    public void handleEndRound(int gameId, String message, int amountOfRounds, int currentRound) {
-        // send end round message to subscribers
-        webSocketService.sendMessageToSubscribers("/topic/games/"+gameId+"/endRound", new EndRoundMessage(message, amountOfRounds, currentRound));
-    }
-
-    @MessageMapping("/games/{gameId}/nextRound")
-    public void nextRound(@DestinationVariable("gameId") int gameId) {
-        RoundService.nextRound(gameId);
-        webSocketService.sendMessageToSubscribers("/topic/games/"+gameId+"/nextRound", new EndRoundMessage("nextRound", 0,0)); // TODO don't rly need to return anything...
-    }
-
-    @MessageMapping("/games/{gameId}/guesses")
-    public void handleGuess(GuessIn guessIn, @DestinationVariable("gameId") int gameId) {
-        logger.info("Handling guess '{}'", guessIn);
-        Date guessTime = new Date(); // guess time is registered at request to evaluate how many points the player gets
-
-        //extract information from JSON
-        String guess = guessIn.getGuess();
-        Player player = playerService.getPlayer(guessIn.getId());
-
-        List<Guess> playerGuesses = RoundService.checkGuessAndAllocatePoints(gameId, player, guess, guessTime);
-
-        webSocketService.sendMessageToSubscribers("/topic/games/"+gameId+"/guesses", playerGuesses);
-
-        RoundService.endRoundIfAllPlayersGuessedCorrectly(this, gameId);
-    }
-
-    @MessageMapping("/games/{gameId}/hints")
-    public void distributeHints(Hint hint, @DestinationVariable("gameId") int gameId){
-        //send hint directly to all subscribers
-        logger.info("Distributing hint '{}' for game {}", hint, gameId);
-        webSocketService.sendMessageToSubscribers("/topic/games/"+gameId+"/hints", hint);
-    }
-
-    @MessageMapping("/games/{gameId}/gameOver")
-    public void endGame(@DestinationVariable("gameId") int gameId){
-        RoundService.handleGameOver(gameId, true);
-        GameService.deleteLobby(gameId, playerService);
-        webSocketService.sendMessageToSubscribers("/topic/games/"+gameId+"/gameOver", new EndRoundMessage("endGame", 0, 0));
-    }
-
-    @MessageMapping("/games/{gameId}/playAgain")
-    public void playAgain(@DestinationVariable("gameId") int gameId){
-        RoundService.handleGameOver(gameId, true);
-        Lobby lobby = GameService.getLobby(gameId);
-        List<Player> players = new ArrayList<>();
-        List<Player> playersOld = lobby.getPlayers();
-        for(Player p : playersOld){
-            players.add(playerService.getPlayer(p.getId()));
+                // Print ID
+                System.out.println("ID: " + id);
+                webSocketService.sendMessageToSubscribers("/topic/games/" + gameId +"/round", id);
+                i = 0;
+            } catch (URISyntaxException | IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        lobby.resetGameToNull(players);
-        webSocketService.sendMessageToSubscribers("/topic/games/"+gameId+"/playAgain", new EndRoundMessage("playAgain", 0, 0));
-        // wait for a second to make sure the players are in the lobby
-        try {
-            Thread.sleep(500);
-            // send a message over websocket to notify the other players who is in the lobby
-            String destination = "/topic/lobbies/" + gameId + "/joined";
-            Lobby l = GameService.getLobby(gameId);
-            LobbyGetDTO lobbyGetDTO = DTOMapper.INSTANCE.convertLobbyToLobbyGetDTO(l);
-            messagingTemplate.convertAndSend(destination, lobbyGetDTO);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 
+    }
 
 }
--- src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Round.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Round.java
@@ -2,7 +2,10 @@ package ch.uzh.ifi.hase.soprafs24.entity;
 
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 //give it
 //{"roundId":"1b5bb058-b7ca-4d3a-88b1-2e05501a03d9","roundNr": 1,"gameId":1b5bb058-b7ca-4d3a-88b1-2e05501a03d9,"guesses":[],"URL":"unsplash.org", "Endtime":"02:48:03"}
@@ -14,7 +17,20 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "ROUND")
-public class Round {
+public class Round implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private Long roundId;
+
+    public Long getRoundId() {
+        return roundId;
+    }
+
+    public void setRoundId(Long id) {
+        this.roundId = roundId;
+    }
 
 }
