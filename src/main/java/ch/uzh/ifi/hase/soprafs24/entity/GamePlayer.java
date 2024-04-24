b41b9638491117791bf891d51e6d432176ebdfa6
Dia Hidvegi <dianahidvegi@Dias-MacBook-Pro-2.local>
2024-04-24 11:08:18 +0200

    added userId and username to GamePlayer return object

--- src/main/java/ch/uzh/ifi/hase/soprafs24/entity/GamePlayer.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/GamePlayer.java
@@ -20,7 +20,7 @@ public class GamePlayer {
     @JoinColumn(name = "game_id")
     private Game game;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User user;
 
--- src/main/java/ch/uzh/ifi/hase/soprafs24/repository/GamePlayerRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/repository/GamePlayerRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 import ch.uzh.ifi.hase.soprafs24.entity.GamePlayer;
+import java.util.List;
+
 
 public interface GamePlayerRepository extends JpaRepository<GamePlayer, Long> {
     Optional<GamePlayer> findByGame_GameIdAndUser_UserId(UUID gameId, Long id);
+    List<GamePlayer> findByPlayerId(Long playerId);
 }
 
