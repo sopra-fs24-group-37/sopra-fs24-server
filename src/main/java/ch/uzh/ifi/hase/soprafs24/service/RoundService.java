b9883af1ea3d0cd16a40f53b8bea8eb36f97ee0f
Dia Hidvegi <dianahidvegi@Dias-MacBook-Pro-2.local>
2024-05-23 00:37:13 +0200

    added condition for null long lat as well

--- src/main/java/ch/uzh/ifi/hase/soprafs24/service/RoundService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/service/RoundService.java
@@ -84,10 +84,12 @@ public class RoundService {
             // Check location data of api call
             JSONObject location = jsonResponse.optJSONObject("location");
             
-            if (location == null || !location.has("position") || 
-                !location.getJSONObject("position").has("latitude") || 
-                !location.getJSONObject("position").has("longitude")) {
-                System.out.println("Image does not have location data. Retrying...");
+            if (!jsonResponse.has("location") || 
+                !jsonResponse.getJSONObject("location").has("position") || 
+                jsonResponse.getJSONObject("location").isNull("position") ||
+                jsonResponse.getJSONObject("location").getJSONObject("position").isNull("latitude") || 
+                jsonResponse.getJSONObject("location").getJSONObject("position").isNull("longitude")) {
+                System.out.println("Image does not have valid location data. Retrying...");
                 return getRandomPicture(round, game);
             }
 
